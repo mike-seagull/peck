50af3ff9587d4365d18750da7c45936b9e8557c5
Michael Hollister <hollister.michael@gmail.com>
2019-02-14 20:56:45 -0600

    bird call

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-scp -o LogLevel=quiet ./bin/whois_checker ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/whois_checker
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/whois_checker"
+scp -o LogLevel=quiet ./bin/domain_pecker ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/domain_pecker
+ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/domain_pecker"
