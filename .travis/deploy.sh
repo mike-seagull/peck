0bfbf68c4895477ff4d79cce219b22112852b734
Mike Hollister <hollister.michael@gmail.com>
2019-02-19 10:16:25 -0600

    moved deployment files to dropbox

--- .travis/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-scp -o LogLevel=quiet ./bin/domain_pecker ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/domain_pecker
-ssh -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} "/bin/chmod +x /usr/local/bin/domain_pecker"
