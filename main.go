b52d0afb326fc8c9141840270f518c6f0f96098a
Michael Hollister <hollister.michael@gmail.com>
2020-03-07 18:48:24 -0600

    added ability to use in lambda function

--- main.go
+++ main.go
@@ -4,30 +4,69 @@ import (
     "github.com/undiabler/golang-whois"
     "github.com/imroc/req"
     "github.com/google/logger"
+    "github.com/aws/aws-lambda-go/lambda"
     "encoding/base64"
     "io/ioutil"
+    "context"
     "fmt"
     "strings"
     "os"
+    "strconv"
+    "errors"
 )
-
-func main() {
-	log := logger.Init("main", true, false, ioutil.Discard)
-	log.Info("starting")
+type Event struct {
+	Domain string `json:"domain"`
+}
+type Response struct {
+	success bool
+	message string
+}
+func IsAvailable(domain string) (bool, error) {
+	log := logger.Init("IsAvailable", true, false, ioutil.Discard)
+	log.Info("inside IsAvailable")
+	if strings.Count(domain, ".") > 1 {
+		// remove subdomain
+		subdomainIndex := strings.Index(domain, ".")
+		domain = domain[subdomainIndex+1:]
+	}
+	if len(domain) > 0 {
+		result, err := whois.GetWhois(domain)
+		if err != nil || len(whois.ParseDomainStatus(result)) == 0 {
+			log.Info(domain+" is available")
+			return true, nil
+		} else {
+			log.Info("got a response. "+domain+" is not available.")
+			return false, nil
+		}
+	} else {
+		return false, errors.New("no domain provided")
+	}
+}
+func LambdaHandler(ctx context.Context, req Event) (Response, error) {
+	log := logger.Init("LambdaHandler", true, false, ioutil.Discard)
+	fmt.Println(ctx)
+	log.Info("in LambdaHandler")
+	fmt.Println(req)
+	is_available, err := IsAvailable(req.Domain)
+	if err != nil {
+		return Response{success: false, message: err.Error()}, err
+	} else if is_available {
+		return Response{success: true, message: req.Domain+" is available"}, nil
+	} else {
+		return Response{success: true, message: "got a response. "+req.Domain+" is not available."}, nil
+	}
+}
+func CommandLine() {
 	args := os.Args[1:]
 	if len(args) < 1 {
 		fmt.Println("Need a domain")
 		os.Exit(1)
 	}
 	domain := args[0]
-	if strings.Count(domain, ".") > 1 {
-		// remove subdomain
-		subdomainIndex := strings.Index(domain, ".")
-		domain = domain[subdomainIndex+1:]
-	}
-	result, err := whois.GetWhois(domain)
-
-	if err != nil || len(whois.ParseDomainStatus(result)) == 0 {
+	log := logger.Init("CommandLine", true, false, ioutil.Discard)
+	log.Info("in CommandLine")
+	is_available, _ := IsAvailable(domain)
+	if is_available {
 		// domain is available
 		log.Info(domain+" is available")
 		home_api_user, user_is_set := os.LookupEnv("HOME_API_USER")
@@ -54,6 +93,21 @@ func main() {
 	} else {
 		// domain is unavailable
 		log.Info("got something back. this domain is in use. going to do nothing.")
+		os.Exit(2)
 	}
-	log.Info("done.")
+}
+func main() {
+	log := logger.Init("main", true, false, ioutil.Discard)
+	log.Info("starting")
+	ISLAMBDA := strings.ToLower(os.Getenv("ISLAMBDA"))
+	is_lambda := false
+	if len(ISLAMBDA) > 0 {
+		is_lambda, _ = strconv.ParseBool(ISLAMBDA)
+	}
+	if is_lambda {
+		// run in lambda function
+		lambda.Start(LambdaHandler)
+	} else {
+		CommandLine()
+	}
 }
